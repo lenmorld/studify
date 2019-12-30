681d608c85a8eba0cec1883d90d25801e114b1dd
lenny <lenmorld@gmail.com>
2019-12-30 10:45:28 -0500

    create POST method and skip-CORS protection

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,29 @@
 class CardsController < ApplicationController
 
+    skip_before_action :verify_authenticity_token
+
     def index
         @cards = Card.all
         render json: @cards
     end
+
+    # TODO: show, new, edit for Admin page
+
+    def create
+        @card = Card.new(card_params)
+
+        if @card.save
+            @cards = Card.all
+            render json: @cards
+        else
+            render json: { error: "Not saved" }
+        end
+    end
+
+    private
+
+        def card_params
+            params.require(:card).permit(:question, :answer)
+        end
+
 end
