5c583f76f8029eba72fa6502e8c47d14b9e3adff
lenmorld <lenmorld@gmail.com>
2020-08-10 20:04:42 -0400

    FlashCard: implement different Q&A presentations for open, multiple, true_false question types

--- src/FlashCard.jsx
+++ src/FlashCard.jsx
@@ -6,22 +6,71 @@ const styles = {
   question: {
     fontSize: "1rem"
   },
-  answer: {
-    fontSize: "1rem"
+  answerContainer: {
+    marginTop: "2rem",
+    fontSize: "0.75rem",
+    border: "1px dashed gray",
+    width: "100%"
   }
 }
 
-const FlashCard = ({ card, answerVisible, toggleAnswerVisible }) => {
-  // const classes = useStyles();
+function Question({ question }) {
+  return (
+    <div style={styles.question} dangerouslySetInnerHTML={{ __html: markdown.render(question) }}></div>
+  )
+}
+
+function Options({ options }) {
+  return options.map(opt => <div key={opt.id}>{opt.description}</div>)
+}
+
+
+function AnswerPresentation({ text }) {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: markdown.render(text) }}></div>
+  )
+}
+
+function Answer({ card }) {
+  const { type, answer, options } = card;
+
+  // if (Array.isArray(answer)) {
+  if (card.type === "multiple") {
+    // 1 answer
+    if (answer.length === 1) {
+      const correctOption = options.find(item => item.id === answer[0]);
+      return <div key={correctOption.id}>{<AnswerPresentation text={correctOption.description} />}</div>
+    } else {
+      // multiple answers
+      const correctOptions = options.filter(item => answer.includes(item.id))
+      return correctOptions.map(co => <div key={co.id}>{<AnswerPresentation text={co.description} />}</div>)
+    }
+    // } else if (typeof answer === "boolean") {
+  } else if (card.type === "true_false") {
+    // true-false
+    return <div>{answer.toString()}</div>
+  }
+
+  // card.type null, default "open"
+  // open-ended question
+  return <AnswerPresentation text={answer} />
+}
+
+const FlashCard = ({ card, answerVisible }) => {
   return (
     <React.Fragment>
-      <div style={styles.question} dangerouslySetInnerHTML={{ __html: markdown.render(card.question) }}>
-      </div>
+      <Question question={card.question} />
       {
-        answerVisible &&
-        <div style={styles.answer} dangerouslySetInnerHTML={{ __html: markdown.render(card.answer) }}></div>
+        card.options && <Options options={card.options} />
       }
-
+      {
+        answerVisible && (<div style={styles.answerContainer}>
+          <Answer card={card} />
+          {
+            card.answer_notes && <div>{card.answer_notes}</div>
+          }
+        </div>)
+      }
     </React.Fragment>
   );
 };
