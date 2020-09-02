91ac709229a730d0d0d1064328cf4679ac50d3df
Lenny LD <lenmorld@gmail.com>
2020-09-01 20:28:40 -0400

    UI: fix env config from require to import

--- src/config.js
+++ src/config.js
@@ -1,9 +1,16 @@
 // init. environment variables
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
+import dotenv from 'dotenv'
 dotenv.config();
 
-module.exports = {
-	node_env: process.env.NODE_ENV,
-	port: process.env.PORT,
-	server_uri: process.env.SERVER_URI
-};
+export default {
+		node_env: process.env.NODE_ENV,
+		port: process.env.PORT,
+		server_uri: process.env.SERVER_URI
+	}
+
+// module.exports = {
+// 	node_env: process.env.NODE_ENV,
+// 	port: process.env.PORT,
+// 	server_uri: process.env.SERVER_URI
+// };
