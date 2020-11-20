4ffc028d8719527a4376e0a5c85b27e47d03485c
Lenny LD <lenmorld@gmail.com>
2020-11-19 21:37:23 -0500

    FlashCard + Markdown fixes

--- src/MarkdownTextArea.jsx
+++ src/MarkdownTextArea.jsx
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 // import { Button } from "@material-ui/core";
-import Button from '../src/components/Button'
+import { Input } from "antd"
+import { SaveOutlined, EditOutlined } from "@ant-design/icons"
 
-import { Input } from 'antd';
-const { TextArea } = Input;
+import Button from "./components/Button"
 
-import Spacer from "./Spacer";
+import Spacer from "./Spacer"
 
-import md from './utils/markdown'
+import md from "./utils/markdown"
+
+const { TextArea } = Input
 
 // TODO: auto size based on view port
 // TODO: preview
 
 /*
     <textarea> props:
-    cols, rows, autosize, placeholder 
+    cols, rows, autosize, placeholder
 */
 
 const defaultProps = {
-  wrap: "off"
+  wrap: "off",
   // rows: "4", cols: "50",
-};
+}
 
 const styles = {
   mainContainer: {
@@ -31,18 +33,18 @@ const styles = {
     // NOTE: or use flex: 1, if parent is flex box
 
     display: "flex",
-    flexDirection: "column",
+    // flexDirection: "column", // NOTE: made as a prop instead
     // alignItems: "flex-start"
     alignItems: "stretch", // fill up cross axis (horizontal)
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   mdContainer: {
     // height: "100%",
     // width: "100%"
-    // flex: 1 // let md take entire space
-    flexBasis: "80%", // leave a bit of space for space-between with button to work
+    flex: 1 // let md take entire space
+    // flexBasis: "80%", // leave a bit of space for space-between with button to work
     // but for mobile we still need the <Spacer />
-    fontSize: "0.75rem",
+    // fontSize: "0.75rem",
   },
   textarea: {
     // height: "inherit", // it would follow container div
@@ -50,7 +52,7 @@ const styles = {
     height: "100%", // fill up the flex: 1 parent
     width: "100%",
 
-    fontSize: "0.65rem",
+    // fontSize: "0.65rem",
     lineHeight: "1.33",
     padding: "0.25rem",
 
@@ -58,13 +60,7 @@ const styles = {
     // borderRadius: "5px",
     // padding: "0.5rem" // internal padding
   },
-  button: {
-    fontSize: '0.75rem',
-    alignSelf: "flex-start", // override stretch in the flex box
-    // backgroundColor: "#1976d2",
-    // color: "white"
-  }
-};
+}
 
 /**
  * if text is given:
@@ -74,59 +70,64 @@ const styles = {
  * Go to EDIT mode, and set button to Disabled until text is given
  */
 
-const MarkdownTextArea = ({ text, readOnly, onMdChange, autoFocus, placeholder }) => {
-  const [editMode, toggleEditMode] = useState(!!text);
-  const [markdown, setMarkdown] = useState(text || "");
+const MarkdownTextArea = ({
+  text,
+  readOnly,
+  onSave,
+  onEdit,
+  autoFocus,
+  placeholder,
+  layout,
+}) => {
+  const [editMode, toggleEditMode] = useState(!!text)
+  const [markdown, setMarkdown] = useState(text || "")
 
-  // TODO: remove onChange and read from button click instead
-  const onChangeMdText = event => {
-    const mdString = event.target.value;
-    const value = mdString || "";
-    console.log("EDIT...", value)
-    setMarkdown(value);
+  const onChangeMdText = (event) => {
+    const mdString = event.target.value
+    const value = mdString || ""
+    // console.log("EDIT...", value)
+    setMarkdown(value)
+  }
 
-    // change state on parent via function prop
-    onMdChange(value)
-  };
+  const handleClick = () => {
+    if (editMode && onEdit) {
+      // save
+      onEdit()
+    } else if (!editMode && onSave) {
+      // edit
+      onSave(markdown)
+    }
 
-  debugger;
+    toggleEditMode((prev) => !prev)
+  }
 
   return (
-    <div style={styles.mainContainer}>
+    <div style={{ ...styles.mainContainer, flexDirection: layout || 'row'}}>
       <div style={styles.mdContainer}>
         {editMode ? (
-          <div dangerouslySetInnerHTML={{ __html: md.render(markdown) }}></div>
+          <div dangerouslySetInnerHTML={{ __html: md.render(markdown) }} />
         ) : (
-            <TextArea
-              placeholder={placeholder}
-              autoFocus={autoFocus}
-              defaultValue={markdown}
-              onChange={onChangeMdText}
-              size="small"
-              style={styles.textarea}
-            />
-            // <textarea
-            //   {...defaultProps}
-            //   {...props}
-            //   style={styles.textarea}
-            //   defaultValue={markdown}
-            //   onChange={onChangeMdText}
-            // ></textarea>
-          )}
+          <TextArea
+            placeholder={placeholder}
+            autoFocus={autoFocus}
+            defaultValue={markdown}
+            onChange={onChangeMdText}
+            size="small"
+            style={styles.textarea}
+          />
+        )}
       </div>
-
-      <Spacer />
 
       <Button
         type="default"
-        size='small'
-        onClick={() => toggleEditMode(!editMode)}
-        style={styles.button}
-      >
-        {editMode ? "Edit Markdown" : "Preview"}
-      </Button>
+        shape="circle"
+        size="small"
+        icon={editMode ? <EditOutlined /> : <SaveOutlined />}
+        onClick={handleClick}
+        style={{ marginTop: layout === 'column' && '10px', marginLeft: layout === 'row' && '10px' }}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default MarkdownTextArea;
+export default MarkdownTextArea
